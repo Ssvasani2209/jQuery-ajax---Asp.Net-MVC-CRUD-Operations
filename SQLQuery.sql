f5d54dfb5e487380ef563c6c5ffb576384b182fb
Ssvasani2209 <saurabh.vasani2209@gmail.com>
2019-05-28 14:16:25 +0530

    Sql Script
    Sql Script Commit

---
+++ SQLQuery.sql
@@ -0,0 +1,24 @@
+ 
+
+/****** Object:  Table [dbo].[Employee]    Script Date: 28-05-2019 14:15:37 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Employee](
+	[EmployeeID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NULL,
+	[Position] [varchar](50) NULL,
+	[Office] [varchar](50) NULL,
+	[Salary] [int] NULL,
+	[ImagePath] [varchar](500) NULL,
+ CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED 
+(
+	[EmployeeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
